1f8e78be33476e45f28526f8c68265bddfdecfdf
PedroBessaRachel <pedro__bessa@outlook.com.br>
2015-07-12 11:36:47 -0300

    Pedro Bessa e Rachel

--- miscellania.class.php
+++ miscellania.class.php
@@ -1,5 +1,14 @@
 <?php
 class Miscellania {
-
+  {
+     public $nome; 
+     public $seno; 
+    public function cateto oposto sobre hipotenusa) {
+     echo "<br/>Confirmado o resultado"; 
+     } 
+     public function($seno){ 
+     echo "<br/>Foi dado o valor de "$seno; 
+ } 
+}
 }
 ?>
